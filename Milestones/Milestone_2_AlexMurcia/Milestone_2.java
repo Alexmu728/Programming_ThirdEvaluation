d8409cadd94ae936d0236436cadc00eaca383d94
Alex <amurciaqu23dw@ikzubirimanteo.com>
2024-03-22 17:55:21 +0100

    created the main panel and implemented the ActionListener

--- Milestones/Milestone_2_AlexMurcia/Milestone_2.java
+++ Milestones/Milestone_2_AlexMurcia/Milestone_2.java
@@ -1,9 +1,26 @@
 package Milestones.Milestone_2_AlexMurcia;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Milestone_2 extends JFrame {
+public class Milestone_2 extends JFrame implements ActionListener {
     public Milestone_2(){
+        super("Test Events: Files");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(800, 600));
+
+        JPanel mainPanel= new JPanel(new BorderLayout());
+
+
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
