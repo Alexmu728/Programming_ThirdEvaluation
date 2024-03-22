ee983ac61b8b3359ff6c3d62f357e5a2e1892a74
Alex <amurciaqu23dw@ikzubirimanteo.com>
2024-03-22 18:02:24 +0100

    Created the comboBox on center and added

--- Milestones/Milestone_2_AlexMurcia/Milestone_2.java
+++ Milestones/Milestone_2_AlexMurcia/Milestone_2.java
@@ -14,13 +14,30 @@ public class Milestone_2 extends JFrame implements ActionListener {
         JPanel mainPanel= new JPanel(new BorderLayout());
 
 
+        //ComboBox-Center
+        String[] fileTextNames={"java.txt", "python.txt", "c.txt"};
+        JComboBox box= new JComboBox(fileTextNames);
+        box.addActionListener(this);
+
+        JPanel center= new JPanel();
+        center.add(box);
 
 
+        //Add main panel
+        mainPanel.add(center, BorderLayout.CENTER);
 
+
+        this.add(mainPanel);
+        this.pack();
+        setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public static void main(String[] args) {
+        Milestone_2 m2= new Milestone_2();
     }
 }
