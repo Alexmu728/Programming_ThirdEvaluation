31ce7f842a7079835e963b10e7879a071c5e7b96
ik012982i9 <jgallardome23dw@ikzubirimanteo.com>
2024-04-08 12:53:27 +0200

    created a JOptionPanel for the password at the beginning

--- Milestones/Milestone_3_AlexMurcia/Milestone_3.java
+++ Milestones/Milestone_3_AlexMurcia/Milestone_3.java
@@ -6,15 +6,12 @@ import java.awt.*;
 public class Milestone_3 extends JFrame {
     public Milestone_3(){
         super("Swing - Example 2");
-        String password= JOptionPane.showInputDialog("Input password");
-        if(password.equals("damocles")) {
+        String password = JOptionPane.showInputDialog("Input password");
+        if(password.equals("damocles")){
             this.setDefaultCloseOperation(EXIT_ON_CLOSE);
             this.setPreferredSize(new Dimension(600, 600));
-
-
         }else{
-            setVisible(false);
-            dispose();
+            System.exit(0);
         }
         this.pack();
         setVisible(true);
