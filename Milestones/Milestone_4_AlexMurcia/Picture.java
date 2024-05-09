2323f1951477f4efe60fe91b57e360395b791d2d
programming <amurciaqu23dw@ikzubirimanteo.com>
2024-05-09 09:40:42 +0200

    Class Picture finished

--- Milestones/Milestone_4_AlexMurcia/Picture.java
+++ Milestones/Milestone_4_AlexMurcia/Picture.java
@@ -1,4 +1,45 @@
 package Milestones.Milestone_4_AlexMurcia;
 
+import java.util.Date;
+
 public class Picture {
+    private int pictureId;
+    private String title;
+    private Date date;
+    private String file;
+    private int visits;
+    private int photographerId;
+
+    public Picture(int pictureId, String title, Date date, String file, int visits, int photographerId){
+        this.pictureId= pictureId;
+        this.title=title;
+        this.date=date;
+        this.file=file;
+        this.visits=visits;
+        this.photographerId=photographerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPictureId() {
+        return pictureId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public int getPhotographerId() {
+        return photographerId;
+    }
 }
